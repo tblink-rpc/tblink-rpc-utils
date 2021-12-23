85f2b2c562f6209c0e50e829b157d789c28a6d5c
Matthew Ballance <matt.ballance@gmail.com>
2021-12-22 19:24:42 -0800

    Begin to add support for generating Verilog BFM code
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/tblink_rpc_utils/gen_utils.py
+++ src/tblink_rpc_utils/gen_utils.py
@@ -7,8 +7,9 @@ from tblink_rpc_utils.type_spec import TypeSpec, TypeKind
 
 class GenUtils(object):
     
-    def __init__(self, cpp_ptr=False):
+    def __init__(self, cpp_ptr=False, vlog=False):
         self.cpp_ptr = cpp_ptr
+        self.vlog = vlog
         if cpp_ptr:
             self.ptr = "->"
         else:
@@ -17,20 +18,30 @@ class GenUtils(object):
         
     def gen_mk_type(self, iftype_b, t : TypeSpec) -> str:
         if t is None:
-            if self.cpp_ptr:
+            if self.cpp_ptr or self.vlog:
                 return "0"
             else:
                 return "null"
         elif t.kind == TypeKind.Int:
-            return "%s%smkTypeInt(%s, %d)" % (
-                iftype_b,
-                self.ptr,
-                self.bool_str(t.is_signed),
-                t.width)
+            if self.vlog:
+                return "$tblink_rpc_IInterfaceTypeBuilder_mkTypeInt(%s, %d, %d)" % (
+                    iftype_b, t.is_signed, t.width)
+            else:
+                return "%s%smkTypeInt(%s, %d)" % (
+                    iftype_b,
+                    self.ptr,
+                    self.bool_str(t.is_signed),
+                    t.width)
         elif t.kind == TypeKind.Bool:
-            return "%s%smkTypeBool()" % (
-                iftype_b,
-                self.ptr)
+            if self.vlog:
+                return "$tblink_rpc_IInterfaceTypeBuilder_mkTypeBool(%s)" % (
+                    iftype_b)
+            else:
+                return "%s%smkTypeBool()" % (
+                    iftype_b,
+                    self.ptr)
+        else:
+            raise Exception("Unsupported type %s" % str(t.kind))
             
     def bool_str(self, v):
         if self.cpp_ptr:
